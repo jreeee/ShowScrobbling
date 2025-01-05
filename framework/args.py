f7f9a8f738bdf6d1e9a2a4ba053de3c12dcf40c3
jreeee <jeremias.michael.kilian@uni-weimar.de>
2025-01-05 23:23:42 +0100

    trying gh actions for linting

--- framework/args.py
+++ framework/args.py
@@ -1,46 +1,50 @@
+"""
+dealing with arguments
+"""
+
 import argparse
-import framework.constants as const
+import constants as const
 
 
-# parsing args and setting default values
 def parse_args():
-  parser = argparse.ArgumentParser(
-      prog="ShowScrobbling", description="displays your scrobbles on discord"
-  )
-  parser.add_argument("-u", "--user", nargs="?", help="your lastfm username")
-  parser.add_argument(
-      "-l",
-      "--loglevel",
-      nargs="?",
-      const=1,
-      type=int,
-      default=1,
-      help="program generated output, 0: silent -> 3: debug, default 1",
-  )
-  parser.add_argument(
-      "-i",
-      "--image",
-      nargs="?",
-      const=1,
-      default=const.DEFAULT_TRACK_IMAGE,
-      help="default image link if there's none for the track",
-  )
-  parser.add_argument(
-      "-c",
-      "--cycle",
-      nargs="?",
-      const=1,
-      type=int,
-      default=10,
-      help="relevant if track length not found, default is 7, then multiplied by request interval",
-  )
-  parser.add_argument(
-      "-r",
-      "--request",
-      nargs="?",
-      const=1,
-      type=int,
-      default=30,
-      help="interval in seconds to request lastfm api for most recent track, default 30",
-  )
-  return parser.parse_args()
+    """parsing args and setting default values"""
+    parser = argparse.ArgumentParser(
+        prog="ShowScrobbling", description="displays your scrobbles on discord"
+    )
+    parser.add_argument("-u", "--user", nargs="?", help="your lastfm username")
+    parser.add_argument(
+        "-l",
+        "--loglevel",
+        nargs="?",
+        const=1,
+        type=int,
+        default=1,
+        help="program generated output, 0: silent -> 3: debug, default 1",
+    )
+    parser.add_argument(
+        "-i",
+        "--image",
+        nargs="?",
+        const=1,
+        default=const.DEFAULT_TRACK_IMAGE,
+        help="default image link if there's none for the track",
+    )
+    parser.add_argument(
+        "-c",
+        "--cycle",
+        nargs="?",
+        const=1,
+        type=int,
+        default=10,
+        help="relevant if track length not found, default = 7, then multiplied by request interval",
+    )
+    parser.add_argument(
+        "-r",
+        "--request",
+        nargs="?",
+        const=1,
+        type=int,
+        default=30,
+        help="interval in seconds to request lastfm api for most recent track, default 30",
+    )
+    return parser.parse_args()
--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ if CONST_FILE.exists():
         print("exting")
         sys.exit(0)
     else:
-        open(CONST_FILE, 'w').close()
+        open(CONST_FILE, "w", encoding='utf-8').close()
 
 USERNAME = input("please enter your lastfm username: ")
 
-with open(CONST_FILE, 'w') as constants:
+with open(CONST_FILE, "w", encoding='utf-8') as constants:
     constants.write("# the following values can be changed\n")
-    constants.write(f"USR = \"{USERNAME}\"\n")
-    constants.write("DEFAULT_TRACK_IMAGE = \"https://media.tenor.com/Hro804BGJaQAAAAj/miku-headbang.gif\"\n")
+    constants.write(f'USR = "{USERNAME}"\n')
+    constants.write('DEFAULT_TRACK_IMAGE = "https://media.tenor.com/Hro804BGJaQAAAAj/miku-headbang.gif"\n')
     constants.write("\n# the following values shouldn't be changed\n")
-    constants.write("LFM_API_KEY = \"5125b622ac7cb502b9a857bb59a57830\"\n")
-    constants.write("LFM_TR_IN_QRY = \"http://ws.audioscrobbler.com/2.0/?method=track.getInfo\"\n")
-    constants.write("MB_REC_QRY = \"https://musicbrainz.org/ws/2/recording/?query=\"\n\n")
-    constants.write("CLIENT_ID = \"1301054835101270117\"\n")
+    constants.write('LFM_API_KEY = "5125b622ac7cb502b9a857bb59a57830"\n')
+    constants.write('LFM_TR_IN_QRY = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo"\n')
+    constants.write('MB_REC_QRY = "https://musicbrainz.org/ws/2/recording/?query="\n\n')
+    constants.write('CLIENT_ID = "1301054835101270117"\n')
     constants.write("URL_RECENT_TRACK = f'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying=\"true\"&user={USR}&limit=1&api_key={LFM_API_KEY}&format=json'\n")
     constants.write("URL_TRACK_INFO = f'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={LFM_API_KEY}&username={USR}'\n")
